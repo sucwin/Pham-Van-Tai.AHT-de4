8be848c586411c1939841430b2081833eb6743f8
sucwin <taipv2x@gmail.com>
2021-10-13 18:36:49 +0700

    khung. màu. light-height. menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
 
+   let flag=0;
     // nav menu
     $('#menu-id').click(function (e) { 
         $('.mobile-nav').removeClass('close')
+        setTimeout(()=>{
+            flag =1
+        },1000)
+       
+        
     });
-
+   
     $('#close-menu').click(function (e) { 
         $('.mobile-nav').addClass('close')
     });
-
 
     // nav products
     $('.product-item-nav').click(function (e) { 
@@ -23,4 +28,15 @@ $(document).ready(function () {
         
     });
     
+    $(document).click(function (e)
+    {
+        var container = $(".mobile-nav");
+        if(flag==1)
+        if (!container.is(e.target) && container.has(e.target).length === 0)
+        {
+            $('.mobile-nav').addClass('close')
+            flag=0;
+        }
+    });
+
 });
